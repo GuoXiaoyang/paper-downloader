92aa8326c562fc9f325032bb4a09293ba886a635
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-04-22 13:08:32 +0800

    fix the bug: cheked the select_all checkbox in IEEE

--- js/ieeeDownload.js
+++ js/ieeeDownload.js
@@ -2,11 +2,13 @@
  * Created by guoxiaoyang on 2017/1/15.
  */
 
+var dashboard=$(".pure-u-14-24");
 
-
-[].map.call($("input:checked"),function (result) {
+[].map.call($("input:checked",dashboard),function (result) {
+    console.log(result,$(result).length);
     var targetItem = $(result).parent().parent();
     var download_a = $(".icon-pdf",targetItem)[0].href;
+    console.log("checked-downloader-links:",download_a);
     var title = $(".pure-u-22-24 h2>a",targetItem).text().replace(/\W/g,' ');
     var date = $(".description>div:eq(0)>span:eq(0)",targetItem).text().match(/\d{4}/g);
     var authors = $(".author",targetItem).text().replace(/[\s\r\n]/g,"");
@@ -16,6 +18,34 @@
     var download_locked = $(".icon-access-locked",targetItem);
     var access = (download_success.length == 1);
     return {filename:filename, download_a:download_a, access:access};
+
+
+
+    // $(result).each(function () {
+
+
+        //get request:cross domain
+/*        $.ajax({
+            type: "GET",
+            url: download_a,
+            dataType: 'html',
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            success:function(data, textStatus){
+                console.log(data);
+
+            },
+            error:function(XMLHttpRequest, textStatus, errorThrown){
+                console.error("Request failure:",textStatus)
+            }
+        });*/
+
+
+
+    // alert(download_a);
+    // });
 
 
 });
