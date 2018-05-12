7a9c1cbca202e395e7e89c7b9fbb9cb21fc1b474
shawpan <shawpan@yeah.net>
2018-05-12 12:54:21 +0800

    new start

--- js/ieeeDownload.js
+++ js/ieeeDownload.js
@@ -2,53 +2,35 @@
  * Created by guoxiaoyang on 2017/1/15.
  */
 
-var dashboard=$(".article-list.List-results");
-// console.log('dashboard', dashboard[0]);
+if($(".results").length) {
+	var dashboard = $(".results");
+	[].map.call($("input:checked",dashboard),function (result) {
+		var targetItem = $(result).parent().parent();
+		var url = $(".button",targetItem).parent()[0].href;
+		var title = $(".txt h3>a>span",targetItem).text().replace(/\W/g,' ');
+		var year = $(".txt",targetItem)[0].innerText.match(/[Publication Year: ](\d{4})/g)[0].trim();
+		var authors = $(".authors",targetItem).text().replace(/[\s\r\n]{2,}/g,"");
+		var journal = $('h1').text().trim();
+		var download_success = $(".access-icon",targetItem);
+		var access = (download_success.length == 1);
+		console.log('title:'+title+'\n url:'+url+'\n authors:'+authors+'\n journal:'+journal+'\n year:'+year+'\n access:'+access);
+		return {title:title, url:url, access:access, year:year, authors:authors, journal:journal};
+	});
+} else {
+	var dashboard=$(".article-list.List-results");
+	[].map.call($("input:checked",dashboard),function (result) {
+		var targetItem = $(result).parent().parent();
+		var url = $(".icon-pdf",targetItem)[0].href;
+		console.log("checked-downloader-links:",url);
+		var title = $(".col-22-24 h2>a",targetItem).text().replace(/\W/g,' ');
+		var year = $(".description>div:eq(0)>span:eq(0)",targetItem).text().match(/\d{4}/g);
+		var authors = $(".author",targetItem).text().replace(/[\s\r\n]/g,"");
+		var journal = $(".description>a:eq(0)",targetItem).text();
+		var download_success = $(".icon-access-subscribed",targetItem);
+		var download_locked = $(".icon-access-locked",targetItem);
+		var access = (download_success.length == 1);
+		console.log('title:'+title+'\n url:'+url+'\n authors:'+authors+'\n journal:'+journal+'\n year:'+year+'\n access:'+access);
+		return {title:title, url:url, access:access, year:year, authors:authors, journal:journal};
+	});
+}
 
-[].map.call($("input:checked",dashboard),function (result) {
-    // console.log('result', result)
-    // console.log('result length',$(result).length);
-    var targetItem = $(result).parent().parent();
-    var download_a = $(".icon-pdf",targetItem)[0].href;
-    console.log("checked-downloader-links:",download_a);
-    var title = $(".col-22-24 h2>a",targetItem).text().replace(/\W/g,' ');
-    var date = $(".description>div:eq(0)>span:eq(0)",targetItem).text().match(/\d{4}/g);
-    var authors = $(".author",targetItem).text().replace(/[\s\r\n]/g,"");
-    var journal = $(".description>a:eq(0)",targetItem).text();
-    var filename = "【"+date+"】"+title+".pdf";
-    console.log('filename', filename);
-    var download_success = $(".icon-access-subscribed",targetItem);
-    var download_locked = $(".icon-access-locked",targetItem);
-    var access = (download_success.length == 1);
-    return {filename:filename, download_a:download_a, access:access};
-
-
-
-    // $(result).each(function () {
-
-
-        //get request:cross domain
-/*        $.ajax({
-            type: "GET",
-            url: download_a,
-            dataType: 'html',
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true,
-            success:function(data, textStatus){
-                console.log(data);
-
-            },
-            error:function(XMLHttpRequest, textStatus, errorThrown){
-                console.error("Request failure:",textStatus)
-            }
-        });*/
-
-
-
-    // alert(download_a);
-    // });
-
-
-});
